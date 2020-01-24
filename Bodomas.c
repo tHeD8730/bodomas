965610a1d76d05858c39f81096b69c90c510bf13
Abhishek <cabhishek608@gmail.com>
2020-01-24 22:38:31 +0530

    created program for calculating bodomas

---
+++ Bodomas.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+char c;
+scanf("%d", &a);
+scanf(" %c", &c);
+scanf("%d", &b);
+switch (c)
+{
+case '+': printf("Sum of both numbers are %d", pass);
+break;
+case '-': printf("Difference of both numbers are %d", pass);
+break;
+case '*': printf("Product of both numbers are %d", pass);
+break;
+case '/': printf("Divide of both numbers are %d", pass);
+break;
+}
+
+}
